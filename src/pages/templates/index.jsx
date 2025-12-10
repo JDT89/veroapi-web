c8133bd7fc7c21774c5a61e5ccdd16e170d3d413
JDT89 <115045921+JDT89@users.noreply.github.com>
2025-12-10 17:49:45 -0600

    Add Templates component with TemplateGrid

---
+++ src/pages/templates/index.jsx
@@ -0,0 +1,9 @@
+import TemplateGrid from '../../components/TemplateGrid'
+export default function Templates(){
+  return (
+    <div>
+      <h2 className="text-2xl mb-4">All Templates</h2>
+      <TemplateGrid />
+    </div>
+  )
+}
