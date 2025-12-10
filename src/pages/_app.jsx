cee76d20865bbb2c6759a5998cffaf2be6c0a0d1
JDT89 <115045921+JDT89@users.noreply.github.com>
2025-12-10 17:46:53 -0600

    Create _app.jsx

---
+++ src/pages/_app.jsx
@@ -0,0 +1,15 @@
+jsx
+import '../styles/globals.css'
+import { SessionProvider } from 'next-auth/react'
+import Navbar from '../components/Navbar'
+
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <Navbar />
+      <main className="container mx-auto p-4">
+        <Component {...pageProps} />
+      </main>
+    </SessionProvider>
+  )
+}
