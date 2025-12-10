dd3082d2c40327da02e1a795c35b50f9e3fffcb3
JDT89 <115045921+JDT89@users.noreply.github.com>
2025-12-10 15:24:13 -0600

    Add routes for Register, ForgotPassword, and ResetPassword

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Docs from './pages/Docs';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import AdminDashboard from './pages/AdminDashboard';
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/docs" element={<Docs />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password/:token" element={<ResetPassword />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
