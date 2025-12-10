4aca366e9712f150b8759e3ff7a2d01a8367c475
JDT89 <115045921+JDT89@users.noreply.github.com>
2025-12-10 17:54:08 -0600

    Create [...nextauth].js

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,18 @@
+import NextAuth from 'next-auth'
+import DiscordProvider from 'next-auth/providers/discord'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import prisma from '../../../lib/prisma'
+
+export default function auth(req, res){
+  return NextAuth(req, res, {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+      DiscordProvider({
+        clientId: process.env.DISCORD_CLIENT_ID,
+        clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      })
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+    session: { strategy: 'jwt' }
+  })
+}
