af342cb50d22c315a59d8829650640864a2636e9
JDT89 <115045921+JDT89@users.noreply.github.com>
2025-12-10 17:51:26 -0600

    Create upload.jsx

---
+++ src/pages/upload.jsx
@@ -0,0 +1,9 @@
+import UploadForm from '../components/UploadForm'
+export default function Upload(){
+  return (
+    <div>
+      <h2 className="text-2xl mb-4">Create Template</h2>
+      <UploadForm />
+    </div>
+  )
+}
