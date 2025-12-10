f47f5384f2b501463f8923b6932da3328829976e
JDT89 <115045921+JDT89@users.noreply.github.com>
2025-12-10 17:54:32 -0600

    Add API route to get user session

---
+++ src/pages/api/me.js
@@ -0,0 +1,8 @@
+import { getServerSession } from 'next-auth/next'
+import authOptions from './auth/[...nextauth]'
+
+export default async function handler(req,res){
+  const session = await getServerSession(req, res, authOptions)
+  if (!session) return res.status(401).json({ user: null })
+  return res.json({ user: session.user })
+}
